b308a13fe93caefa8bb5a84cefe03f69cbb39534
Valky-7 <kauan_gramkow@estudante.sesisenai.org.br>
2026-04-15 21:10:24 -0300

    Direcionamento dos botões agora funcionando.

--- principal/src/app/app.ts
+++ principal/src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from './footer/footer';
 import { Home } from './home/home';
 import { Catalogo } from './catalogo/catalogo';
 import { Promocoes } from './promocoes/promocoes';
+import { Contato } from './contato/contato';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Home, Catalogo, Promocoes],
+  imports: [RouterOutlet, Header, Footer, Home, Catalogo, Promocoes, Contato],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
